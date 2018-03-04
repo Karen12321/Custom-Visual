4ba226b6eb6df7f9f68ba06a00d29001533ecbfe
Karen12321 <37034076+Karen12321@users.noreply.github.com>
2018-03-04 19:24:33 +1100

    initial update

---
+++ myCustomVisual/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var myCustomVisual4B7378D7B0FA4AA79F318AE13164CA34_DEBUG = {
+        name: 'myCustomVisual4B7378D7B0FA4AA79F318AE13164CA34_DEBUG',
+        displayName: 'MyCustomVisual',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.10.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.myCustomVisual4B7378D7B0FA4AA79F318AE13164CA34.Visual(options),
+        custom: true
+    };
+}
